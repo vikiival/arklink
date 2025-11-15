7fa7ef2124acd6402371313be50645fb6ece4534
Viki Val <viktorko99@gmail.com>
2025-11-15 20:56:46 -0300

    :zap: add remote pattern for pbs.twimg.com in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
     ],
   },
 };
